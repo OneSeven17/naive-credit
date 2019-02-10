6279561c6c8b0bfe84fb07c49c031b58758041e5
OneSeven17 <17renatis@gmail.com>
2019-02-10 10:33:48 +0300

    добавил в отслеживаемые файлы

---
+++ app/lib.py
@@ -0,0 +1,29 @@
+def is_creditable(age, salary):
+    # первый тест позитивный - если все данные валидны, то результат позитивный
+    """
+    >>> is_creditable(30, 40_000)
+    True
+
+    >>> is_creditable(20, 40_000)
+    False
+
+    >>> is_creditable(70, 40_000)
+    False
+
+    >>> is_creditable(30, 10_000)
+    False
+    """
+    min_age = 21
+    max_age = 60
+    min_salary = 30_000
+
+    if age < min_age:
+        return False # Early Exit (быстрый выход)
+    # если был return, то до этой точки мы не дойдем
+    if age > max_age:
+        return False # Early Exit
+    if salary < min_salary:
+        return False
+
+    # в этой точке все проверки пройдены
+    return True
